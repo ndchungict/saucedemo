04fac9755f6dda02d9d2bb78976e3fbf1a4cbcbb
NguyenChung <ndchungict@gmail.com>
2021-07-19 22:48:22 +0700

    connect online devices

--- src/test/java/chungnd/demo/runner/LoginTestSuite.java
+++ src/test/java/chungnd/demo/runner/LoginTestSuite.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
         features = "src\\test\\resources\\features\\login",
-//        tags = "@App",
+        tags = "@App",
         glue = "chungnd.demo.steps"
 )
 public class LoginTestSuite {
