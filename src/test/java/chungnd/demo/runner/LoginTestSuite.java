dd54eb0dc9b5e936b7fc2867dab9baa4ccd9a7d8
NguyenChung <ndchungict@gmail.com>
2021-07-19 22:54:28 +0700

    edit os version and testsuite

--- src/test/java/chungnd/demo/runner/LoginTestSuite.java
+++ src/test/java/chungnd/demo/runner/LoginTestSuite.java
@@ -7,7 +7,6 @@ import org.junit.runner.RunWith;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
         features = "src\\test\\resources\\features\\login",
-        tags = "@App",
         glue = "chungnd.demo.steps"
 )
 public class LoginTestSuite {
